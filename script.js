676f8e103039de91f84d10cf6a0bcdb70eb979e1
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-17 21:02:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+//your JS code here. If required
+	var select =document.getElementById('colorSelect');
+	var removebtn=document.getElementById('btn');
+
+	btn.addEventListener('click', function()){
+		const selectedval=select.value;
+
+		const options =select.options;
+		for (let i = 0; i < options.length; i++) {
+			if(options[i].value===selectedval){
+				colorSelect[i].remove(i);
+				break;
+			}
+		}
+	});
+
